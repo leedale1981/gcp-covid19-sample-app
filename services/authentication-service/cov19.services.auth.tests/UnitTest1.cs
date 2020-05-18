3df8c244eecbed12a17fbbf707484dddbe2cc492
Lee Dale <lee.dale@ofqual.gov.uk>
2020-05-18 18:37:06 +0100

    Initial commit

---
+++ services/authentication-service/cov19.services.auth.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace cov19.services.auth.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
