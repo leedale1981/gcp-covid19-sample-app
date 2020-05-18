4dce0039ad2bf5fecfbae43190b38ff0f8bf6073
Lee Dale <lee.jdale@gmail.com>
2020-05-18 21:42:10 +0100

    Adding kubernetes build

--- deployment/kubernetes/create-cluster.sh
+++ deployment/kubernetes/create-cluster.sh
@@ -1,1 +1,1 @@
-gcloud container clusters create c19-sample-web-services-cluster --zone europe-west2-a --machine-type=f1-micro --labels=app=
+gcloud container clusters create c19-sample-web-services-cluster --zone europe-west2-a --labels=app=cov19-sample-app
